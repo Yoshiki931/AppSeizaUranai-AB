76985e404601aae8e75f0d713922e18536150669
Yoshiki Nakamura <yoshiki.nakamura.sys@gmail.com>
2017-07-11 11:59:36 +0900

    frist commit

---
+++ src/Uranai.java
@@ -0,0 +1,86 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Uranai {
+
+  int point;
+  int total;
+  static Scanner sc;
+  static Random rand;
+
+  public static void main(String[] args) {
+    sc = new Scanner( System.in );
+    rand = new Random();
+
+
+  }
+
+  /*
+   * 【皆さんにお願いしたいこと】
+   *  星座の順位（１～１２位）に応じて占い結果（文字）を表示する.
+   *
+   *  内容に関しては、好きにしてください。
+   *  例を下に乗せておきます。
+   *  コピーするなりして頑張ってください。
+   *  内容に関して、迷った時はめざましテレビの占いでも参考にしてください。
+   */
+
+  public void Aries( int num ){
+    String seiza = "牡羊座";
+    switch( num ){
+
+      // case 1 = 一位の時のメッセージ
+      case 1:
+        System.out.println( num + "位" + seiza);
+        System.out.println("故意のビッグチャンス到来でモテモテ！");
+        System.out.println("気になる人には積極的なアピールを！");
+        System.out.println("ラッキーポイント：『インテリア雑貨ショップ』");
+        System.out.println("");
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+      case 4:
+        break;
+      case 5:
+        break;
+      case 6:
+        break;
+      case 7:
+        break;
+      case 8:
+        break;
+      case 9:
+        break;
+      case 10:
+        break;
+      case 11:
+        break;
+      case 12:
+    }
+    return;
+  }
+  public void Taurus( int num ){
+  }
+  public void Gemini( int num ){
+  }
+  public void Cancer( int num ){
+  }
+  public void Leo( int num ){
+  }
+  public void Virgo( int num ){
+  }
+  public void Libra( int num ){
+  }
+  public void Scorpio( int num ){
+  }
+  public void Sagittarius( int num ){
+  }
+  public void Capricorn( int num ){
+  }
+  public void Aquarius( int num ){
+  }
+  public void Pisces( int num ){
+  }
+}
